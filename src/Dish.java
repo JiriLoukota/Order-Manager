3f4c1bbbdbe7fd70af79d7bc9c3cb8095122174e
Jiří Loukota <jloukota@seznam.cz>
2024-03-02 11:13:41 +0100

    Created class Dish to store info about dishes.

---
+++ src/Dish.java
@@ -0,0 +1,59 @@
+package orderManager;
+
+import orderManager.exceptions.InvalidDataException;
+
+import java.math.BigDecimal;
+
+public class Dish {
+    private String title, image;
+    private BigDecimal price;
+    private int preparationTime;//in minutes
+
+    public Dish(String title, String image, BigDecimal price, int preparationTime) throws InvalidDataException {
+        this.title = title;
+        this.image = image;
+        setPrice(price);
+        setPreparationTime(preparationTime);
+    }
+
+    public Dish(String title, BigDecimal price, int preparationTime) throws InvalidDataException {
+        this(title, "blank", price, preparationTime);
+    }
+    //region Getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) throws InvalidDataException {
+        if(price.compareTo(BigDecimal.valueOf(0)) > 0) this.price = price;
+        else throw new InvalidDataException(getClass() + "; value should be zero or bigger number.");
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(int preparationTime) throws InvalidDataException {
+        if(preparationTime > 0) this.preparationTime = preparationTime;
+        else throw new InvalidDataException(getClass() + "; value should be higher than zero.");
+    }
+
+    //endregion
+}
